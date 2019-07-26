2f548420d9911f5c15c174bd6d498af85123945b
Jeff Wu <jtzwu@google.com>
2019-07-26 09:28:20 -0700

    Create a working app

---
+++ main.py
@@ -0,0 +1,28 @@
+import flask
+import google.cloud.datastore
+import os
+
+datastore_client = google.cloud.datastore.Client()
+app = flask.Flask(__name__)
+
+@app.route('/hello')
+def hello():
+  return "Hello World!\n"
+
+@app.route('/view')
+def view():
+  query = datastore_client.query(kind='test-entity')
+  names = ('* {name}\n'.format(name=person['Name']) for person in query.fetch())
+  return flask.Response(names, mimetype='text/plain')
+
+@app.route('/add/<user>', methods=['PUT'])
+def add(user):
+  key = datastore_client.key('test-entity')
+  person = google.cloud.datastore.Entity(key=key)
+  person['Name'] = user
+  datastore_client.put(person)
+
+  return "Hello {name}\n".format(name=user)
+
+if __name__ == '__main__':
+  app.run('0.0.0.0', port=6666)
